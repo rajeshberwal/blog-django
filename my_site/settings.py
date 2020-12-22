b2c2d4739054f30084b5e5aa55d443a23d607ad3
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-12-22 18:56:53 +0530

    still can't figure out the problem with this application

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import os
+
 from pathlib import Path
 import environ
 import django_heroku
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
