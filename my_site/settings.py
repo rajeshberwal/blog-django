95da4f33f47fcbb85f749e977e09961d3d2b8e3f
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-12-22 19:48:23 +0530

    trying to log the reason

--- my_site/settings.py
+++ my_site/settings.py
@@ -134,3 +134,19 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+       'console': {
+           'class': 'logging.StreamHandler',
+       },
+   }, 
+   'loggers': {
+       'django': {
+           'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+       },
+   },
+}
