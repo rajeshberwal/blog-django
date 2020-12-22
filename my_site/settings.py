792056afa59147f4b98972e634b5dc9817f9f639
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-12-22 19:18:31 +0530

    settings verson: unknown

--- my_site/settings.py
+++ my_site/settings.py
@@ -12,17 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 
 from pathlib import Path
-import environ
 import django_heroku
 import os
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# reading env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
