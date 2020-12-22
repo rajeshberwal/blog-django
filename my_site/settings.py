ed87d93ee837e64aa2ce7fa61775574699034fee
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-12-22 16:47:49 +0530

    configured for heroku

--- my_site/settings.py
+++ my_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku
 
 env = environ.Env(
     # set casting, default value
@@ -135,3 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
