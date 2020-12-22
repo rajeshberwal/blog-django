d9c886f1ba70ad053ffb30ba9a1a371d05ad5054
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-12-22 17:34:01 +0530

    updated settings for deployment

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 import django_heroku
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
